f6f965697b0718db23a73603166c828d85af62e2
zizero0102 <geyoo0102@naver.com>
2025-09-01 18:01:33 +0900

    장바구니 2차 수정
    장바구니 2차 수정

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import './App.css';
-import Install from './install';
-import StarRating from './components/StarRating';
-import Cart from './cart/Cart'
 
-import { Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Install from "./install";
+import StarRating from "./components/StarRating";
+import Cart from "./cart/Cart";
+import Login from "./login/Login";
+
+import { Route, Routes } from "react-router-dom";
+
+
+
 
 function App() {
   return (
     <div className="App">
-      
 
       <Routes>
-        <Route path='/' element={
-          <>
-          <h1>PWA 테스트</h1>
-          <Install />
-          <StarRating itemId={1} />
-          <StarRating itemId={2} />
-          <StarRating itemId={3} />
-          </>
-        }>
-        </Route>
-        <Route path='/Cart' element={<Cart />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <h1>PWA 테스트</h1>
+              <Install />
+              <StarRating itemId={1} />
+              <StarRating itemId={2} />
+              <StarRating itemId={3} />
+            </>
+          }
+        ></Route>
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/Login" element={<Login />} />
+
+
       </Routes>
     </div>
   );
--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Cart() {
-    return <h1>장바구니</h1>
+    return (
+    <>
+        <h1>장바구니</h1>
+        <div>장바구니 추가내용</div>
+    </>
+    )
 }
 
 export default Cart;
